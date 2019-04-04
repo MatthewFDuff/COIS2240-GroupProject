9117c78427ea4141db1f371db7fcb950febfb886
Rylan Crooks <rcwyatt917@gmail.com>
2019-04-04 17:37:52 -0400

    Commenting Delete & Edit taskControllers
    +Added more comments to DeleteTaskController and EditTaskCOntroller

--- src/productivityplanner/ui/deletetask/DeleteTaskController.java
+++ src/productivityplanner/ui/deletetask/DeleteTaskController.java
@@ -25,37 +25,40 @@ public class DeleteTaskController implements Initializable {
     @FXML
     private BorderPane rootPane;
 
-    Task task;
+    Task task;                                               // Focused task up for deletion
 
     @Override
+    // Setup the delete window
     public void initialize(URL location, ResourceBundle resources) {
-        task = TaskCellController.getSelected().getTask();
-        if (task != null)
+        task = TaskCellController.getSelected().getTask();           // Set focused task
+        if (task != null)                                            // If the task exists...
         {
-            txtName.setText(task.getName());
+            txtName.setText(task.getName());                         // Update the Edit form to display the task's pre-edit information.
             colourPicker.setValue(task.getColor());
-            colourPicker.setOpacity(1.0);
-        }
+            colourPicker.setOpacity(1.0);                            //Since the colour picker isn't in use, it fades the colour
+        }                                                            //This line counteracts that change
     }
 
     @FXML
+    // Runs once the confirm delete is pressed
     private void deleteTask(ActionEvent event) {
 
-        // Update the task's information in the database.
-        if (DatabaseHelper.deleteTask(task)) {
-            cancel(new ActionEvent()); // Close the window when the task has been updated.
-        }else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
+
+        if (DatabaseHelper.deleteTask(task)) {              // Update the task's information in the database.
+            cancel(new ActionEvent());                      // Close the window when the task has been updated.
+        }else {                                             // If It can not delete task
+            Alert alert = new Alert(Alert.AlertType.ERROR); // Create Alert for failing to delete
             alert.setHeaderText(null);
-            alert.setContentText("Failed.");
+            alert.setContentText("Failed to delete task.");
             alert.showAndWait();
             System.out.println("Error: Unable to delete task.");
         }
     }
 
     @FXML
+    // Closes the window, Runs if cancel is hit and after deletion
     public void cancel(ActionEvent event) {
-        Stage stage = (Stage) rootPane.getScene().getWindow();
-        stage.close();
+        Stage stage = (Stage) rootPane.getScene().getWindow(); // Get access to the window
+        stage.close();                                         // Close the window
     }
 }
